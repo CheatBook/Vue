6f9aa08a6adf7f83191a5625aee7b18391e56c6d
Unknown <km1994321@yahoo.co.jp>
2020-01-29 23:35:50 +0900

    keyupの記述

--- No.7_イベントハンドラ_2/main.js
+++ No.7_イベントハンドラ_2/main.js
@@ -1,11 +1,18 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: ''
+        message: '',
+        message2: '',
     },
     methods: {
         clickHandler: function () {
             this.message = new Date().toLocaleTimeString()
+        },
+        clear: function () {
+            this.message2 = ''
+        },
+        clickHandlerAlert: function () {
+            alert('shift + click')
         }
     }
 
