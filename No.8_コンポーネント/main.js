f184678e928771368232b7910deae398165fda2f
Unknown <km1994321@yahoo.co.jp>
2020-01-30 15:28:13 +0900

    コンポーネント関連の実装

---
+++ No.8_コンポーネント/main.js
@@ -0,0 +1,29 @@
+var helloComponent = {
+    template: '<p>Hello2</p>'
+}
+
+
+Vue.component('hello-component', {
+    template: '<p>Hello</p>'
+})
+
+Vue.component('button-counter', {
+    data: function () {
+        return {
+            count: 0
+        }
+    },
+    template: '<button v-on:click="count++">{{ count }}</button>'
+})
+
+var app = new Vue({
+    el: '#app',
+    data: {
+    },
+    methods: {
+    },
+    components: {
+        'hello-component2': helloComponent
+    }
+
+})
