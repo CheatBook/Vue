c37fd476c79d0fb43f49f42ccb0599f47dfa3a21
Unknown <km1994321@yahoo.co.jp>
2020-01-31 23:30:04 +0900

    VueCLIでの実装

---
+++ No.11_CLI/my-first-vue-cli-project/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloNuxt/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloNuxt from '@/components/HelloNuxt.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloNuxt
+  }
+}
+</script>
