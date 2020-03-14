8e49e190c3d68775f827e4f5e9066cdc749c2c6d
Unknown <km1994321@yahoo.co.jp>
2020-03-14 16:03:34 +0900

    axiosと認証

--- No.13_axios/udemy-vuejs/src/main.js
+++ No.13_axios/udemy-vuejs/src/main.js
@@ -1,8 +1,31 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import router from './router'
 
 Vue.config.productionTip = false
 
+axios.defaults.baseURL = "https://firestore.googleapis.com/v1/projects/vuejs-http-56b19/databases/(default)/documents";
+
+axios.interceptors.request.use(
+  config => {
+    console.log(config)
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+axios.interceptors.response.use(
+  response => {
+    console.log(response)
+    return response;
+  },
+  error => {
+    return Promise.reject(error);
+  });
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ No.13_axios/udemy-vuejs/src/views/Login.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h2>ログイン</h2>
+    <label for="email">Email：</label>
+    <input type="text" id="email" v-model="email" />
+    <br />
+    <br />
+    <label for="password">パスワード：</label>
+    <input id="password" type="password" v-model="password" />
+    <br />
+    <br />
+    <button @click="login">送信</button>
+    <h2>掲示板</h2>
+  </div>
+</template>
+
+<script>
+import axios from "../axios-auth";
+
+export default {
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    login() {
+      axios
+        .post(
+          "/accounts:signInWithPassword?key=AIzaSyCU0bpIGZ-2knWb_3NAnrEiOXxqvyFUnXQ",
+          {
+            email: this.email,
+            password: this.password,
+            returnSecureToken: true
+          }
+        )
+        .then(response => {
+          console.log(response);
+        });
+      this.email = "";
+      this.password = "";
+    }
+  }
+};
+</script>
